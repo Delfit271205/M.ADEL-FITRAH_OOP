4737f4c6f7b00948b6a645daf01eadd3cdbcce4e
M.ADEL FITRAH <madelfitrah690@gmail.com>
2025-12-04 09:19:41 +0800

    Add files via upload

---
+++ tugas2.py
@@ -0,0 +1,16 @@
+#m.adel fitrah-24241153
+class Kendaraan:
+    pemilik = "adel"
+    def __init__(self, jenis, merk, tahun):
+        self.jenis = jenis
+        self.merk = merk
+        self.tahun = tahun
+
+    def info(self):
+        return f"Jenis: {self.jenis}, Merk: {self.merk}, Tahun: {self.tahun}"
+
+mobil = Kendaraan("Mobil", "Toyota", 2020)
+print(mobil.info())
+motor = Kendaraan("Motor", "Honda", 2019)
+print(motor.info())
+print(f"Pemilik Kendaraan: {Kendaraan.pemilik}")
